5dd57add8a82bdfac2a5857e65254ea2ebd91487
Responsibleuk <james@we-are-responsible.com>
2023-06-14 14:33:16 +0000

    Add

--- js/preloader.js
+++ js/preloader.js
@@ -1,10 +1,7 @@
-window.addEventListener('load', function() {
-    var preloader = document.getElementById('preloader');
-    var content = document.getElementById('content');
-
-    var video = document.getElementById('preloader-video');
-    video.addEventListener('canplaythrough', function() {
-      preloader.style.display = 'none';
-      content.style.display = 'block';
-    });
-  });
+window.addEventListener("load", function () {
+  const preloader = document.getElementById("preloader");
+  
+  setTimeout(function() {
+    preloader.style.display = "none";
+  }, 5000);
+});
